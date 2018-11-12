a0dc4b56e119fd2599b467f80f3de77a4b8a8725
DELL-DAVID\NOUVEAU <davidlaroche@hotmail.fr>
2018-11-12 12:43:01 +0100

    Blocs PHP : bannière movie + header

---
+++ bannerMovie.php
@@ -0,0 +1,10 @@
+<section class="sectionHeader">
+    <article class="texteTopHeader">
+        <h1><?php echo $data['title']; ?></h1>
+        <p><time datetime="2017-10-04">(2017)</time><p>
+    </article>
+    <div class="texteBackHeader">
+        <p class="note">7.4/10</p>
+    </div>
+    <img class="background" src="img/1.jpg" alt="" />
+</section>
---
+++ header.php
@@ -0,0 +1,11 @@
+<header>
+    <a href="index.php"><img src="img/origami.png" alt=""/></a>
+    <a href="index.php"><h1>SensCritik</h1></a>
+    <nav>
+        <ul>
+            <li><a href="index.php">Film</a></li>
+            <li><a href="actor.php">Acteurs</a></li>
+            <li><a href="director.php">Directeurs</a></li>
+        </ul>
+    </nav>
+</header>
