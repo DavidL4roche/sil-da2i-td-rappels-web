f8a4f222e828a6ad8b1e668f3e974f9d32dd5f9e
DELL-DAVID\NOUVEAU <davidlaroche@hotmail.fr>
2018-11-12 12:42:09 +0100

    Mise en place pages principales du site

---
+++ director.php
@@ -0,0 +1,108 @@
+<?php
+    require 'config.php';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>SensCritik - Réalisateur</title>
+	<link rel="stylesheet" href="style.css" />
+	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+</head>
+<body>
+
+    <?php
+        getBlock('header');
+    ?>
+	
+	<main>
+		<section>
+			<article>
+				<h1>Denis Villeneuve</h1>
+				<p><time datetime="1967-10-03">Né le 3 octobre 1967</time><p>
+				<img src="img/Denis_Villeneuve.jpg" alt="" />
+			</article>
+			
+			<article>
+				<h2>Biographie</h2>
+				<h3>Jeunesse et formation</h3>
+				<p>
+					Denis Villeneuve a fait ses études secondaires au Séminaire Saint-Joseph de Trois-Rivières, ses études collégiales en sciences de la nature au Cégep de Trois-Rivières, puis ses études universitaires en cinéma à l'Université du Québec à Montréal.
+					En 1990-1991, il remporte la Course Europe-Asie grâce à ses reportages audacieux. Produit et diffusé par la télévision de Radio-Canada, le concours réserve à son gagnant un billet pour réaliser un film avec l'Office national du film du Canada (ONF).
+				</p>
+				<h3>Débuts de réalisateur (1988-1996)</h3>
+				<p>
+					Après avoir accompagné Pierre Perrault dans le Grand Nord canadien pour son tout dernier film, Cornouailles, Villeneuve réalise son premier film professionnel avec l'ONF, sur un thème imposé fourni par l'Agence canadienne de développement international : le multiculturalisme. Tourné en Jamaïque, REW FFWD raconte l'histoire d'un photographe retenu par une panne d'auto dans un ghetto réputé dangereux.
+					En 1996, il scénarise et réalise un des segments du film collectif Cosmos, sélectionné à la Quinzaine des réalisateurs au Festival de Cannes 1997 et récipiendaire du prix international des cinémas d’art et d’essai.
+				</p>
+				<h3>Révélation critique du cinéma canadien (1998-2010)</h3>
+				<p>
+					En 1998, il signe le scénario et la réalisation de son premier long-métrage, Un 32 août sur Terre, présenté dans plus de trente-cinq festivals internationaux. Le film fait notamment partie des sélections officielles de Cannes et Toronto. Un 32 août sur Terre est aussi retenu pour représenter le Canada aux Oscars 1999 dans la catégorie du « Meilleur film en langue étrangère ».
+					En 2000, il écrit et réalise son deuxième long métrage, Maelström, sélectionné par une quarantaine de festivals, dont le Festival du film de Sundance et le Festival international du film de Toronto. Le film récolte plus de vingt-cinq prix à travers le monde, parmi lesquels le Prix de la Critique Internationale (FISPRESCI) au Festival de Berlin, le Prix SACD pour le meilleur scénario, ainsi que le Prix Génie du meilleur film au Canada. Maelström représente à son tour le Canada aux Oscars en 2001.
+					Il a réalisé un court métrage de fiction intitulé Next Floor qui, après avoir été présenté lors de la Semaine de la critique au Festival de Cannes en mai 2008, a remporté le Grand Prix Canal+ du meilleur court-métrage.
+					Il confirme en signant le long-métrage Polytechnique, portant sur la tuerie de l'École Polytechnique de Montréal, présenté à la Quinzaine des réalisateurs lors du 62e Festival de Cannes.
+					L'année suivante, il dévoile Incendies, l’adaptation cinématographique de la pièce à succès de Wajdi Mouawad, dont la première a eu lieu à la Mostra de Venise. Le film est nommé aux Oscars 2011 dans la catégorie du « meilleur film en langue étrangère ». Il a remporté 8 prix Génie ainsi que 9 prix Jutra, notamment ceux du meilleur film et de la meilleure réalisation. Incendies est considéré par le New York Times comme un des 10 meilleurs films de 20111.
+				</p>
+				<h3>Passage à Hollywood (2013-)</h3>
+				<p>
+					Villeneuve au Festival de Cannes 2015.
+					L'année 2013 marque son arrivée sur le marché hollywoodien : il est derrière les thrillers psychologiques Prisoners, puis Enemy (bien que ce dernier ait été tourné en premier, déjà avec Jake Gyllenhaal). Les deux longs-métrages sont présentés au Festival international du film de Toronto 2013, et sont acclamés par la critique. Prisoners est nommé à l'Oscar de la meilleure photographie en 2014.
+					Lors du Festival de Cannes 2015, son thriller d'action Sicario, réunissant Emily Blunt, Josh Brolin et Benicio del Toro est en compétition pour la Palme d'or. Le film est nommé aux Oscars dans 3 catégories, notamment celle de la meilleure photographie (une nouvelle fois pour Roger Deakins). Benicio del Toro est nommé au BAFTA du meilleur acteur dans un second rôle.
+					Cette trilogie de thrillers noirs bouclés, il se lance dans la science-fiction - et des plus gros budgets - avec le tournage de Premier Contact, adapté d'une nouvelle de Ted Chiang. Il y dirige les stars Amy Adams et Jeremy Renner dans les rôles principaux. Le film est dévoilé à la Mostra de Venise en 2016, puis reçoit 8 nominations aux Oscars en 2017, dont celle du meilleur film et de la meilleure réalisation2.
+					Il enchaîne avec une suite très attendue en étant choisi pour mettre en scène Blade Runner 2049, suite du classique Blade Runner. Villeneuve est annoncé au poste de réalisateur en février 2015 par Alcon Entertainment. Harrison Ford y reprend son rôle, face à un nouveau héros incarné par Ryan Gosling. Ridley Scott, le réalisateur du premier film, se cantonne cette fois au rôle de producteur3. Le long-métrage sort à la rentrée 2017.
+					Le 31 janvier 2017, le studio Legendary annonce que Denis Villeneuve sera le réalisateur de Dune, nouvelle adaptation du roman éponyme de Frank Herbert4.
+					En septembre 2017, il rèvèle que le rôle tenu par Jared Leto dans Blade Runner 2049 devait être tenu par David Bowie qui est décédé le 10 janvier 2016 pendant la pré-production du film donc Jared Leto l'a remplacé. En octobre 2017, Blade Runner 2049 sort en salle, le film est très bien accueilli par la critique mais connaît un succès commercial mitigé.
+					En 2018, il fait partie du jury du Festival de Cannes, présidé par Cate Blanchett, aux côtés des actrices Léa Seydoux et Kristen Stewart, de la réalisatrice Ava DuVernay, de la chanteuse Khadja Nin, de l'acteur Chang Chen et des réalisateurs Robert Guédiguian et Andreï Zviaguintsev.
+				</p>
+			</article>
+			
+			<article>
+				<h2>Filmographie</h2>
+					<h3>Réalisateur</h3>
+						<h4>Longs métrages</h4>
+							<ul>
+								<li>1998 : Un 32 août sur terre</li>
+								<li>2000 : Maelström</li>
+								<li>2009 : Polytechnique</li>
+								<li>2010 : Incendies</li>
+								<li>2013 : Prisoners</li>
+								<li>2013 : Enemy</li>
+								<li>2015 : Sicario</li>
+								<li>2016 : Premier Contact (Arrival)</li>
+								<li>2017 : Blade Runner 2049</li>
+							</ul>
+						<h4>Courts métrages</h4>
+							<ul>
+								<li>1994 : REW FFWD (moyen-métrage documentaire)</li>
+								<li>1996 : Cosmos (segment "Le Technétium" - film collectif)</li>
+								<li>2008 : Next Floor (court-métrage)</li>
+								<li>2011 : Rated R for Nudity (court-métrage)</li>
+							</ul>
+					<h3>Scénariste</h3>
+						<ul>
+							<li>1996 : Cosmos</li>
+							<li>1998 : Un 32 août sur terre</li>
+							<li>2000 : Maelström</li>
+							<li>2009 : Polytechnique</li>
+							<li>2010 : Incendies</li>
+						</ul>
+					<h3>Acteur</h3>
+						<ul>
+							<li>1995 : Zigrail de André Turpin : Denis</li>
+						</ul>
+					<h3>Producteur</h3>
+						<ul>
+							<li>2018 : Sicario: La Guerre des cartels (Sicario: Day of the Soldado) de Stefano Sollima</li>
+						</ul>
+			</article>
+			
+		</section>
+	</main>
+	
+	<footer>
+		<p><a href="https://www.joebertin.fr">David LAROCHE</a></p>
+	</footer>
+	
+</body>
+</html>
